a8993e5f7f85e5b759ced188e13ccaa7371e7823
mikdec33 <mikayladec@icloud.com>
2025-11-20 10:08:52 -0800

    delete filler files

--- routes/export.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { exportExcel } = require("../controllers/exportController");
-
-router.get("/:budgetId", exportExcel);
-
-module.exports = router;
--- routes/register.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { register } = require("../controllers/authController");
-
-// POST /register
-router.post("/", register);
-
-module.exports = router;
